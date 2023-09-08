35b5ce572d0e775f956fbe50523bf215ee4a8a8a
Maurice-NYAB <maurice.adjeibaah@hotmail.com>
2023-09-07 17:49:15 -0700

    C printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - the actual function for printing
+ *
+ * Return: 0 at end
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
