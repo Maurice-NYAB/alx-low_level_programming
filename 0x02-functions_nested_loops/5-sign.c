b90d7e64e7b77548825ba5f6524e469014479d84
Maurice-NYAB <maurice.adjeibaah@hotmail.com>
2023-09-12 18:33:44 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign and number after function
+ *@n: variable to be used
+ * Return: sign or number
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
