f40e7355e377f791c4f2608c64677f9608f05364
Maurice-NYAB <maurice.adjeibaah@hotmail.com>
2023-09-14 18:37:08 -0700

    printing diagonally

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal-prints diagonal backward slash
+ * @n: number of times '\' needs to be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int a, b;
+
+		for (a = 0; a < n; a++)
+		{
+			for (b = 0; b <= a; a++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
