5cc1e1705d85428e8f680a96f8ffafa2502fcc20
Maurice-NYAB <maurice.adjeibaah@hotmail.com>
2023-09-22 13:35:09 -0700

    printing a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -18,10 +18,12 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
+
 	while (o < size)
 	{
 		j = size - o < 10 ? size - o : 10;
 		printf("%08x: ", o);
+
 		for (i = 0; i < 10; i++)
 		{
 			if (i < j)
@@ -41,7 +43,7 @@ void print_buffer(char *b, int size)
 			{
 				c = '.';
 			}
-			printf("%o", c);
+			printf("%c", c);
 		}
 		printf("\n");
 		o += 10;
