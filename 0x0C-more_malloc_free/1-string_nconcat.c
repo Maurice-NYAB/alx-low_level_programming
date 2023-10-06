9ff89dd5c23fc5ca81b8c652a393bb425961c409
Maurice-NYAB <maurice.adjeibaah@hotmail.com>
2023-10-05 18:53:30 -0700

    concatenate strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -12,9 +12,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *prnt_ptr;
 	unsigned int len_s1, len_s2;
 
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
-
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	len_s1 = 0;
 	while
 		(s1[len_s1] != '\0') {
@@ -28,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n >= len_s2)
 		n = len_s2;
-	prnt_ptr = malloc((len_s1 + n + 1) * sizeof(char));
+	prnt_ptr = malloc((len_s1 + n) * sizeof(char) + 1);
 	if (prnt_ptr == NULL)
 		return (NULL);
 	len_s1 = 0;
