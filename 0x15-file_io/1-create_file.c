7a27afdfef6e124df0ef4afde2429026f6e6daec
Maurice-NYAB <maurice.adjeibaah@hotmail.com>
2023-10-30 21:20:55 -0700

    Under the snow

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -25,7 +25,7 @@ int create_file(const char *filename, char *text_content)
 		text_content = "";
 
 	for (letters = 0; text_content[letters]; letters++)
-
+		;
 	con_wr = write(file_des, text_content, letters);
 
 	if (con_wr == -1)
