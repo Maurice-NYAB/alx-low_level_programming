9ace6a4b72aa3e04c03d914ed2280af7417e417c
Maurice-NYAB <maurice.adjeibaah@hotmail.com>
2023-10-30 21:19:29 -0700

    Speak gently, she can hear

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,7 +25,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (text_content)
 	{
 		for (letters = 0; text_content[letters]; letters++)
-
+			;
 		con_wr = write(file_des, text_content, letters);
 
 		if (con_wr == -1)
